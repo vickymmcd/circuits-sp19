663a8e45963b78af99e095f5e60037832f1239e4
gracemontagnino <grace.montagnino@students.olin.edu>
2019-03-02 10:25:54 -0500

    exp1 plot

---
+++ lab 3/exp1/consts.py
@@ -0,0 +1,54 @@
+from scipy import stats
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# Exp 1 Data
+Exp1_Ib = open('Ib.txt', 'r').read().split()
+Exp1_Ie = open('Ie.txt', 'r').read().split()
+Exp1_Vb = open('Vb.txt', 'r').read().split()
+linear_v = []
+linear_i = []
+beta_vals = []
+
+V_bVals = []
+I_cVals = []
+
+i = 0
+for x in Exp1_Ib:
+    I_bval = float(x)
+    I_eval = float(Exp1_Ie[i])
+    V_bval = float(Exp1_Vb[i])
+    if V_bval > .4 and V_bval < .75:
+        linear_v.append(V_bval)
+        linear_i.append(np.log(float((-1*I_eval) - I_bval)))
+    V_bVals.append(V_bval)
+    I_cVals.append((-1*I_eval) - I_bval)
+    #beta_vals.append(((-1*I_eval) - I_bval)/I_bval)
+    i = i+1
+
+slope, intercept, r_value, p_value, std_err = stats.linregress(linear_v, linear_i)
+
+U_t = (1/(slope))
+I_s = np.exp(intercept)
+
+i = 0
+for x in Exp1_Ib:
+    x = float(x)
+    beta = (I_s/x)*np.exp(V_bVals[i]/U_t)
+    beta_vals.append(beta)
+    i+=1
+
+beta = np.mean(beta_vals)
+
+
+
+if __name__ == '__main__':
+    Data = plt.plot(V_bVals, beta_vals, 'bo', markersize=3, label="300 Ohm Resistor")
+
+    print(U_t)
+    #print(I_s)
+    #print(beta_vals)
+    #print(beta)
+plt.show()
---
+++ lab 3/exp1/plot1.py
@@ -0,0 +1,65 @@
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+from scipy import stats
+import matplotlib.ticker as plticker
+
+Exp1_Ib = open('../exp1/Ib.txt', 'r').read().split()
+Exp1_Ie = open('../exp1/Ie.txt', 'r').read().split()
+Exp1_Vb = open('../exp1/Vb.txt', 'r').read().split()
+i = 0
+Exp1_Ic_Theo = []
+Exp1_Ic = []
+Exp1_Ib_Theo = []
+
+for x in Exp1_Ib:
+    I_bval = float(x)
+    I_eval = float(Exp1_Ie[i])
+    Exp1_Ib[i] = I_bval
+    Exp1_Ie[i] = I_eval
+    Exp1_Vb[i] = float(Exp1_Vb[i])
+    Exp1_Ic.append((-1*I_eval) - I_bval)
+    print("appending")
+    i = i+1
+linear_v = []
+linear_i = []
+beta_vals = []
+
+V_bVals = []
+I_cVals = []
+
+# plt.figure(0)
+# title = "Current Gain"
+# xLabel = "Collector Current (A)"
+# yLabel = "Base Current (A)"
+# Data = plt.semilogy(Exp1_Ic, Exp1_Ib , 'k-', markersize=3, label="none")
+# plt.legend()
+# plt.xlabel(xLabel)
+# plt.ylabel(yLabel)
+# plt.title(title)
+# loc = plticker.MultipleLocator(base=100) # this locator puts ticks at regular intervals
+# plt.grid(True)
+# plt.show()
+slope, intercept, r_value, p_value, std_err = stats.linregress(Exp1_Vb,Exp1_Ic)
+U_t = (1/slope)
+for x in Exp1_Vb:
+     Exp1_Ib_Theo.append(1e-15*math.exp(x/U_t*1000))
+
+for x in Exp1_Vb:
+     Exp1_Ic_Theo.append(1e-12*math.exp(x/U_t*1000))
+if __name__ == '__main__':
+ plt.figure(0)
+ title = "Collector Current and Base Current vs Base Voltage"
+ xLabel = "Voltage In (V)"
+ yLabel = "Current(A)"
+ Data = plt.semilogy(Exp1_Vb, Exp1_Ic , 'k-', markersize=3, label="Collector Characteristic w/o Resistor")
+ Data1 = plt.semilogy(Exp1_Vb, Exp1_Ic_Theo , 'p-', markersize=3, label="Theoretical Collector Characteristic w/o Resistor")
+ Data = plt.semilogy(Exp1_Vb, Exp1_Ib , 'b-', markersize=3, label="Base Characteristic w/o Resistor")
+ Data1 = plt.semilogy(Exp1_Vb, Exp1_Ib_Theo , 'p-', markersize=3, label="Theoretical Base Characteristic w/o Resistor")
+ plt.legend()
+ plt.xlabel(xLabel)
+ plt.ylabel(yLabel)
+ plt.title(title)
+ loc = plticker.MultipleLocator(base=100) # this locator puts ticks at regular intervals
+ plt.grid(True)
+ plt.show()
