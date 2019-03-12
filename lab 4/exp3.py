be9f173d19336e5b5fc1e14a2c46bfea86acc2d8
vickymmcd <vickymmcd@gmail.com>
2019-03-12 19:12:05 -0400

    more data, etc.

--- lab 4/exp3.py
+++ lab 4/exp3.py
@@ -7,6 +7,13 @@ Iz1 = open('exp32/2.94k-sink/Iz.txt', 'r').read().split()
 Iz2 = open('exp32/28k-sink/Iz.txt', 'r').read().split()
 Iz3 = open('exp32/294k-sink/Iz.txt', 'r').read().split()
 
+iy1 = .23/(2940)
+iy2 = .23/(28000)
+iy3 = .23/(294000)
+
+iz_theo1 = []
+iz_theo2 = []
+iz_theo3 = []
 
 i = 0
 for x in Ix:
@@ -18,18 +25,21 @@ i = 0
 for x in Iz1:
     val = -1*float(x)
     Iz1[i]= val
+    iz_theo1.append(Ix[i]**2/iy1)
     i = i+1
 
 i = 0
 for x in Iz2:
     val = -1*float(x)
     Iz2[i]= val
+    iz_theo2.append(Ix[i]**2/iy2)
     i = i+1
 
 i = 0
 for x in Iz3:
     val = -1*float(x)
     Iz3[i]= val
+    iz_theo3.append(Ix[i]**2/iy3)
     i = i+1
 
 
@@ -46,6 +56,10 @@ if __name__ == '__main__':
     Data = plt.loglog(Ix, Iz1, 'bo', markersize=3, label="2.94K Ohms")
     Data = plt.loglog(Ix, Iz2, 'ro', markersize=3, label="28K Ohms")
     Data = plt.loglog(Ix, Iz3, 'go', markersize=3, label="294K Ohms")
+
+    Data = plt.loglog(Ix, iz_theo1, 'b-', markersize=3, label="Iy1 theoretical")
+    Data = plt.loglog(Ix, iz_theo2, 'r-', markersize=3, label="Iy2 theoretical")
+    Data = plt.loglog(Ix, iz_theo3, 'g-', markersize=3, label="Iy3 theoretical")
 
 
 
--- lab 4/exp32.py
+++ lab 4/exp32.py
@@ -2,24 +2,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import stats
 
-Iy = open('exp32/ix1-source/Iy.txt', 'r').read().split()
-Iz1 = open('exp32/ix1-source/Iz.txt', 'r').read().split()
-Iz2 = open('exp32/ix2-source/Iz.txt', 'r').read().split()
-Iz3 = open('exp32/ix3-source/Iz.txt', 'r').read().split()
+Iy = open('exp3/2500k-source/Iy.txt', 'r').read().split()
+Iz1 = open('exp3/2500k-source/Iz.txt', 'r').read().split()
+#Iz2 = open('exp32/ix2-source/negative/Iz.txt', 'r').read().split()
+#Iz3 = open('exp32/ix3-source/negative/Iz.txt', 'r').read().split()
 
-ix1 = 1.0042*10**-6
-ix2 = 1.0042*10**-5
-ix3 = 1.0042*10**-4
-#ix1 = 4.8421*10**-8
-#ix2 = 4.8421*10**-7
-#ix3 = 4.8421*10**-6
+ix1 = 1.0*10**-6
+ix2 = 1.0*10**-5
+ix3 = 1.0*10**-4
+
 iz_theo1 = []
 iz_theo2 = []
 iz_theo3 = []
 
 i = 0
 for x in Iy:
-    val = float(x)
+    val = -1*float(x)
     Iy[i]= val
     i = i+1
 
@@ -28,29 +26,24 @@ for x in Iz1:
     val = -1*float(x)
     Iz1[i]= val
     iz_theo1.append(ix1**2/Iy[i])
-    i = i+1
-
-i = 0
-for x in Iz2:
-    val = -1*float(x)
-    Iz2[i]= val
     iz_theo2.append(ix2**2/Iy[i])
-    i = i+1
-
-i = 0
-for x in Iz3:
-    val = -1*float(x)
-    Iz3[i]= val
     iz_theo3.append(ix3**2/Iy[i])
     i = i+1
 
-iz_theo1.reverse()
-iz_theo2.reverse()
-iz_theo3.reverse()
+# i = 0
+# for x in Iz2:
+#     val = -1*float(x)
+#     Iz2[i]= val
+#     iz_theo2.append(ix2**2/Iy[i])
+#     i = i+1
+#
+# i = 0
+# for x in Iz3:
+#     val = -1*float(x)
+#     Iz3[i]= val
+#     iz_theo3.append(ix3**2/Iy[i])
+#     i = i+1
 
-
-#slope, intercept, r_value, p_value, std_err = stats.linregress(linear_v, linear_i)
-#linear_v = np.array(linear_v)
 
 
 
@@ -60,8 +53,8 @@ if __name__ == '__main__':
     yLabel = "Iz (A)"
 
     Data = plt.loglog(Iy, Iz1, 'bo', markersize=3, label="Ix1= "+str(ix1))
-    Data = plt.loglog(Iy, Iz2, 'ro', markersize=3, label="Ix2= "+str(ix2))
-    Data = plt.loglog(Iy, Iz3, 'go', markersize=3, label="Ix3= "+str(ix3))
+    #Data = plt.loglog(Iy, Iz2, 'ro', markersize=3, label="Ix2= "+str(ix2))
+    #Data = plt.loglog(Iy, Iz3, 'go', markersize=3, label="Ix3= "+str(ix3))
 
     Data = plt.loglog(Iy, iz_theo1, 'b-', markersize=3, label="Ix1 theoretical")
     Data = plt.loglog(Iy, iz_theo2, 'r-', markersize=3, label="Ix2 theoretical")
