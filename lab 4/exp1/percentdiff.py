64cbadf1c3d780e3fb24248db2c6039fe2f5093c
gracemontagnino <grace.montagnino@students.olin.edu>
2019-03-13 15:28:22 -0400

    percentdiff

--- lab 4/exp1/percentdiff.py
+++ lab 4/exp1/percentdiff.py
@@ -12,32 +12,53 @@ pd1=[]
 pd2=[]
 pd3=[]
 pd4=[]
-for i in range(0,len(I_cVals1)):
-    avg=(I_cVals1[i]+I_cVals2[i]+I_cVals3[i]+I_cVals4[i])
+Exp1_Vb1=[]
+I_cVals11=[]
+I_cVals21=[]
+I_cVals31=[]
+I_cVals41=[]
+for x in Exp1_Vb:
+    y = float(x)
+    Exp1_Vb1.append(y)
+for x in I_cVals1:
+    y = float(x)
+    I_cVals11.append(y)
+for x in I_cVals2:
+    y = float(x)
+    I_cVals21.append(y)
+for x in I_cVals3:
+    y = float(x)
+    I_cVals31.append(y)
+for x in I_cVals4:
+    y = float(x)
+    I_cVals41.append(y)
+
+for i in range(0,len(I_cVals11)):
+    avg=(I_cVals11[i]+I_cVals21[i]+I_cVals31[i]+I_cVals41[i])
     avg1=avg*.25
     mean.append(avg1)
-for k in range(0,len(I_cVals2)):
-    pd=(I_cVals2[k]-mean[k])/mean[k]
+for k in range(0,len(I_cVals21)):
+    pd=((I_cVals21[k]-mean[k])/mean[k])*100
     pd2.append(pd)
-for k in range(0,len(I_cVals3)):
-    pd=(I_cVals3[k]-mean[k])/mean[k]
+for k in range(0,len(I_cVals31)):
+    pd=((I_cVals31[k]-mean[k])/mean[k])*100
     pd3.append(pd)
-for k in range(0,len(I_cVals4)):
-    pd=(I_cVals4[k]-mean[k])/mean[k]
+for k in range(0,len(I_cVals41)):
+    pd=((I_cVals41[k]-mean[k])/mean[k])*100
     pd4.append(pd)
-for k in range(0,len(I_cVals1)):
-    pd=(I_cVals1[k]-mean[k])/mean[k]
+for k in range(0,len(I_cVals11)):
+    pd=((I_cVals11[k]-mean[k])/mean[k])*100
     pd1.append(pd)
 
 if __name__ == '__main__':
     plt.figure(0)
-    title = "Beta versus Ib"
-    xLabel = "Beta"
-    yLabel = "Base Current(A)"
-    Data = plt.plot(Exp1_Vb , pd1, 'bo', markersize=3, label="Q1")
-    Data = plt.plot(Exp1_Vb ,pd2,  'ro', markersize=3, label="Q2")
-    Data = plt.plot(Exp1_Vb, pd3 , 'go', markersize=3, label="Q3")
-    Data = plt.plot( Exp1_Vb ,pd4, 'ko', markersize=3, label="Q4")
+    title = "Percent Difference- Ic per transistor and mean of all Ics"
+    xLabel = "Base Voltage (V)"
+    yLabel = "Percent Difference"
+    Data = plt.plot(Exp1_Vb1 , pd1, 'bo', markersize=3, label="Q1 Ic percent difference")
+    Data = plt.plot(Exp1_Vb1 ,pd2,  'ro', markersize=3, label="Q2 Ic percent difference")
+    Data = plt.plot(Exp1_Vb1, pd3 , 'go', markersize=3, label="Q3 Ic percent difference")
+    Data = plt.plot( Exp1_Vb1 ,pd4, 'ko', markersize=3, label="Q4 Ic percent difference")
 
     plt.legend()
     plt.xlabel(xLabel)
