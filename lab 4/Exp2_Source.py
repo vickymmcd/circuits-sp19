f325950017099255e821abfe167b7375d298a739
Allison Basore <allisonbasore@Allisons-MacBook-Air.local>
2019-03-12 23:06:01 -0400

    adding sink and source for exp 2 and added exp1 for loop

--- lab 4/Exp2_Source.py
+++ lab 4/Exp2_Source.py
@@ -9,14 +9,14 @@ from scipy import stats
 
 # Importing Data
 #100 Ohm
-I_y1Temp = open('exp22/294k-sink/Ix.txt', 'r').read().split()
-I_z1Temp = open('exp22/294k-sink/Iz.txt', 'r').read().split()
+I_y1Temp = open('exp2/2500k-source/Iy.txt', 'r').read().split()
+I_z1Temp = open('exp2/2500k-source/Iz.txt', 'r').read().split()
 
-I_y2Temp = open('exp22/28.0k-sink/Ix.txt', 'r').read().split()
-I_z2Temp = open('exp22/28.0k-sink/Iz.txt', 'r').read().split()
+I_y2Temp = open('exp2/250k-source/Iy.txt', 'r').read().split()
+I_z2Temp = open('exp2/250k-source/Iz.txt', 'r').read().split()
 
-I_y3Temp = open('exp22/2.94k-sink/Ix.txt', 'r').read().split()
-I_z3Temp = open('exp22/2.94k-sink/Iz.txt', 'r').read().split()
+I_y3Temp = open('exp2/25k-source/Iy.txt', 'r').read().split()
+I_z3Temp = open('exp2/25k-source/Iz.txt', 'r').read().split()
 
 I_y1 = []
 I_z1 = []
@@ -35,19 +35,18 @@ I_x1 = 0.000001
 
 i = 0
 for x in I_y1Temp:
-    I_yVal = float(x)
+    I_yVal = -1*float(x)
     I_zVal = -1*float(I_z1Temp[i])
     I_y1.append(I_yVal)
     I_z1.append(I_zVal)
     Theo_Z1.append(np.sqrt(I_yVal*I_x1))
-
     i = i + 1
 
 I_x2 = 0.00001
 
 i = 0
 for x in I_y2Temp:
-    I_yVal = float(x)
+    I_yVal = -1*float(x)
     I_zVal = -1*float(I_z2Temp[i])
     I_y2.append(I_yVal)
     I_z2.append(I_zVal)
@@ -55,9 +54,10 @@ for x in I_y2Temp:
     i = i + 1
 
 I_x3 = 0.0001
+
 i = 0
 for x in I_y3Temp:
-    I_yVal = float(x)
+    I_yVal = -1*float(x)
     I_zVal = -1*float(I_z3Temp[i])
     I_y3.append(I_yVal)
     I_z3.append(I_zVal)
@@ -67,24 +67,24 @@ for x in I_y3Temp:
 # print(math.sqrt(0.000001*I_x))
 
 # Setting up plot
-title = "Translinear cirucit output current (Iz) verse input current (Iy)"
+title = "Translinear cirucit output current (Iz) verse input current (Ix)"
 yLabel = "Iz (A)"
-xLabel = "Iy (A)"
+xLabel = "Ix (A)"
 
 # Plotting Data
 
 Data1 = plt.loglog(I_z1, I_y1, 'ro', markersize=3, label="Ix = 1e^-6")
-Data2 = plt.loglog(Theo_Z1, I_y1, 'r--', markersize=3, label="Theoreical 1e^-6")
+Data2 = plt.loglog(Theo_Z1, I_y1, 'r--', markersize=3, label="Iz = 1e^-6 *Ix")
 Data3 = plt.loglog(I_z2, I_y2, 'go', markersize=3, label="Ix = 1e^-5")
-Data4 = plt.loglog(Theo_Z2, I_y2, 'g--', markersize=3, label="Theoreical 1e^-5")
+Data4 = plt.loglog(Theo_Z2, I_y2, 'g--', markersize=3, label="Iz = 1e^-5 *Ix")
 Data5 = plt.loglog(I_z3, I_y3, 'bo', markersize=3, label="Ix = 1e^-4")
-Data6 = plt.loglog(Theo_Z3, I_y3, 'b--', markersize=3, label="Theoreical 1e^-4")
+Data6 = plt.loglog(Theo_Z3, I_y3, 'b--', markersize=3, label="Iz = 1e^-4 *Ix")
 
 plt.xlabel(xLabel)
 plt.ylabel(yLabel)
 plt.title(title)
 plt.legend()
-plt.savefig('Exp2_Ix3_Source.png', format='png')
+plt.savefig('Exp2_Source.png', format='png')
 plt.show()
 
 
--- lab 4/exp1/bvsIb.py
+++ lab 4/exp1/bvsIb.py
@@ -12,16 +12,41 @@ Exp2_Ib = open('two/Ib.txt', 'r').read().split()
 Exp3_Ib = open('three/Ib.txt', 'r').read().split()
 Exp4_Ib = open('four/Ib.txt', 'r').read().split()
 
+Ib1 = []
+Ib2 = []
+Ib3 = []
+Ib4 = []
+
+
+print(beta1)
+
+for x in Exp1_Ib:
+    I_bVal = float(x)
+    Ib1.append(I_bVal)
+
+for x in Exp2_Ib:
+    I_bVal = float(x)
+    Ib2.append(I_bVal)
+
+
+for x in Exp3_Ib:
+    I_bVal = float(x)
+    Ib3.append(I_bVal)
+
+
+for x in Exp4_Ib:
+    I_bVal = float(x)
+    Ib4.append(I_bVal)
 
 if __name__ == '__main__':
     plt.figure(0)
     title = "Beta versus Ib"
     xLabel = "Beta"
     yLabel = "Base Current(A)"
-    Data = plt.semilogx(Exp1_Ib ,beta1, 'bo', markersize=3, label="Q1")
-    Data = plt.semilogx(Exp2_Ib , beta2,'ro', markersize=3, label="Q2")
-    Data = plt.semilogx(Exp3_Ib , beta3,'go', markersize=3, label="Q3")
-    Data = plt.semilogx(Exp4_Ib , beta4,'ko', markersize=3, label="Q4")
+    Data = plt.semilogx(Ib1 , beta1, 'bo', markersize=3, label="Q1")
+    Data = plt.semilogx(Ib2 , beta2,'ro', markersize=3, label="Q2")
+    Data = plt.semilogx(Ib3 , beta3,'go', markersize=3, label="Q3")
+    Data = plt.semilogx(Ib4 , beta4,'ko', markersize=3, label="Q4")
 
     plt.legend()
     plt.xlabel(xLabel)
