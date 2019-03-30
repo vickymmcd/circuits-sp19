cca9711e6170b2000b4bfa5fad1846d20dfb071a
gracemontagnino <grace.montagnino@students.olin.edu>
2019-03-29 21:00:59 -0400

    exp2

--- lab 5/exp2/pMossemilog.py
+++ lab 5/exp2/pMossemilog.py
@@ -1,12 +1,12 @@
 import sys
 sys.path.append('..')
 from linefit import linefit
-#from ekvfit import ekvfit
 import numpy as np
 import math
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 from scipy import stats
+
 nIout = open('nmosexp2/Iout.txt', 'r').read().split()
 nVin = open('pmosexp2/Vin.txt', 'r').read().split()
 pIout = open('pmosexp2/Iout.txt', 'r').read().split()
@@ -19,26 +19,20 @@ i = 0
 print (pIout)
 print(pVin)
 for x in nIout:
-    nI.append(float(x))
-    nV.append(float(nVin[i]))
-    #pI.append(float(pIout[i])*-1)
-    #pV.append(float(pVin[i]))
+    #nI.append(float(x)*-1)
+    #nV.append(float(nVin[i]))
+    pI.append(float(pIout[i])*-1)
+    pV.append(float(pVin[i]))
     i+=1
-title = "Current as a function of the Source Voltage in nMOS"
+print (pV)
+print (pI)
+title = "Current as a function of the Source Voltage in pMOS"
 yLabel = "Current"
 xLabel = "Source Voltage"
-print(len(nV))
-print(len(nI))
-[ss,s,sss,kk,k]=linefit(np.array(nV),np.array(nI),.001)
-Data1 = plt.semilogx(nV, nI, 'ro', markersize=3)
+Data1 = plt.semilogx(pV, pI, 'ro', markersize=3)
 plt.xlabel(xLabel)
 plt.ylabel(yLabel)
 plt.title(title)
 plt.legend()
 plt.savefig('Exp2nMOSsemi.png', format='png')
 plt.show()
-
-
-
-
-#
