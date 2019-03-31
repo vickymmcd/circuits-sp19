b211d59dc8711434693db70bc3101bf74bcfa6b0
vickymmcd <vickymmcd@gmail.com>
2019-03-30 22:21:13 -0400

    adding intrinsic gain plot

--- lab 5/exp3/nmos_gain.py
+++ lab 5/exp3/nmos_gain.py
@@ -3,8 +3,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from nmos_drain import Isat, ro, Isat2, ro2, Isat3, ro3
+from nmos_earlyv import Va, Va2, Va3
 
 Is = 9.78e-8
+Ut = .0258
+
+gain = []
+for i, val in enumerate(Isat):
+    Isat[i] = np.exp(val)
+    gain.append((np.sqrt(np.exp(val)*Is)*Va[i])/(np.exp(val)*Ut))
+
+gain3 = []
+for i, val in enumerate(Isat3):
+    Isat3[i] = np.exp(val)
+    gain3.append(Va[i]/Ut)
 
 if __name__ == '__main__':
 
@@ -15,9 +27,9 @@ if __name__ == '__main__':
 
     # Plotting Data
 
-    Data1 = plt.semilogx(Isat, Va, 'ro', markersize=3, label="Vg=5V (Strong Inversion)")
-    Data1 = plt.semilogx(Isat2, Va2, 'bo', markersize=3, label="Vg=.8V (Moderate Inversion)")
-    Data1 = plt.semilogx(Isat3, Va3, 'go', markersize=3, label="Vg=.7V (Weak Inversion)")
+    Data1 = plt.loglog(Isat, gain, 'ro', markersize=3, label="Vg=5V (Strong Inversion)")
+    #Data1 = plt.semilogx(Isat2, Va2, 'bo', markersize=3, label="Vg=.8V (Moderate Inversion)")
+    Data1 = plt.semilogx(Isat3, gain3, 'go', markersize=3, label="Vg=.7V (Weak Inversion)")
 
 
     plt.xlabel(xLabel)
