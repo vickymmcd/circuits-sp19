44d906a381017ca84fb1fbe41aa7b62d777c86c7
Allison Basore <allisonbasore@Allisons-MacBook-Air.local>
2019-04-12 09:01:19 -0400

    added graph for above

--- lab 7/below_threshold/plotI.py
+++ lab 7/below_threshold/plotI.py
@@ -37,11 +37,16 @@ Sum3 = []
 Sum4 = []
 Sum5 = []
 
+BestFitLine = []
+
 i = 0
+I1Shift = 20
+
 for x in I1V3Raw:
-    I1V3.append(float(x))
-    Vdm3.append(float(VdmV3Raw[i]))
+    if(i>I1Shift):
+        I1V3.append(float(x))
     I2V3.append(float(I2V3Raw[i]))
+    Vdm3.append(float(VdmV3Raw[i]))
 
     I1V4.append(float(I1V4Raw[i]))
     Vdm4.append(float(VdmV4Raw[i]))
@@ -50,32 +55,92 @@ for x in I1V3Raw:
     I1V5.append(float(I1V5Raw[i]))
     Vdm5.append(float(VdmV5Raw[i]))
     I2V5.append(float(I2V5Raw[i]))
-
-    Diff3.append(float(I1V3Raw[i])-float(I2V3Raw[i]))
+    if(i>I1Shift):
+        Diff3.append(float(I1V3Raw[i])-float(I2V3Raw[i]))
     Diff4.append(float(I1V4Raw[i])-float(I2V4Raw[i]))
     Diff5.append(float(I1V5Raw[i])-float(I2V5Raw[i]))
 
-    Sum3.append(float(I1V3Raw[i])+float(I2V3Raw[i]))
+    if(i>I1Shift):
+        Sum3.append(float(I1V3Raw[i])+float(I2V3Raw[i]))
     Sum4.append(float(I1V4Raw[i])+float(I2V4Raw[i]))
     Sum5.append(float(I1V5Raw[i])+float(I2V5Raw[i]))
 
     i+=1
 
 
+gDM3Diff = np.diff(Vdm3)
+I3DiffRaw = np.diff(Diff3)
+I3Diff = []
+
+gDM4Diff = np.diff(Vdm4)
+I4DiffRaw = np.diff(Diff4)
+I4Diff = []
+
+
+gDM5Diff = np.diff(Vdm5)
+I5DiffRaw = np.diff(Diff5)
+I5Diff = []
+
+I1lastVal = I1V3[i-I1Shift-2]
+
+for m in I3DiffRaw:
+    I3Diff.append(m)
+
+for m in I4DiffRaw:
+    I4Diff.append(m)
+
+for m in I5DiffRaw:
+    I5Diff.append(m)
+
+for x in range(0,I1Shift+1):
+    I1V3.append(None)
+    Diff3.append(None)
+    Sum3.append(None)
+    # I3Diff.append(0)
+
+gmn3 = []
+line3 = []
+
+gmn4 = []
+line4 = []
+
+gmn5 = []
+line5 = []
+
+for n in I3Diff:
+    gmn3.append(n/gDM3Diff[0])
+
+for n in I4Diff:
+    gmn4.append(n/gDM4Diff[0])
+
+for n in I5Diff:
+    gmn5.append(n/gDM5Diff[0])
+
+slope3 = gmn3[round(len(gmn3)/2)]
+
+slope4 = gmn4[round(len(gmn4)/2)]
+
+slope5 = gmn5[round(len(gmn5)/2)]
+
+for xx in Vdm4:
+    line3.append(xx*slope3)
+    line4.append(xx*slope4)
+    line5.append(xx*slope5)
 
 if __name__ == "__main__":
     # Setting up plot
-    title = "Current as a function of Vdm"
+    title = "Current as a function of Vdm Below Threshold"
     yLabel = "Current (I)"
     xLabel = "Voltage Differnce Vdm (V)"
 
-    Data1 = plt.plot(Vdm3, I1V3, 'ro', markersize=3, label="I1, V2 = 3")
+
     Data2 = plt.plot(Vdm4, I1V4, 'ko', markersize=3, label="I1, V2 = 4")
-    Data3 = plt.plot(Vdm5, I1V3, 'bo', markersize=3, label="I1, V2 = 5")
+    Data3 = plt.plot(Vdm5, I1V5, 'bo', markersize=3, label="I1, V2 = 5")
+    Data1 = plt.plot(Vdm4, I1V3, 'ro', markersize=3, label="I1, V2 = 3")
 
     Data4 = plt.plot(Vdm3, I2V3, 'r*', markersize=3, label="I2, V2 = 3")
     Data5 = plt.plot(Vdm4, I2V4, 'k*', markersize=3, label="I2, V2 = 4")
-    Data6 = plt.plot(Vdm5, I2V3, 'b*', markersize=3, label="I2, V2 = 5")
+    Data6 = plt.plot(Vdm5, I2V5, 'b*', markersize=3, label="I2, V2 = 5")
 
     Data7 = plt.plot(Vdm3, Sum3, 'r--', markersize=3, label="I1 - I2, V2 = 3")
     Data8 = plt.plot(Vdm4, Sum4, 'k--', markersize=3, label="I1 - I2, V2 = 4")
@@ -84,6 +149,11 @@ if __name__ == "__main__":
     Data7 = plt.plot(Vdm3, Diff3, 'r^', markersize=3, label="I1 + I2, V2 = 3")
     Data8 = plt.plot(Vdm4, Diff4, 'k^', markersize=3, label="I1 + I2, V2 = 4")
     Data9 = plt.plot(Vdm5, Diff5, 'b^', markersize=3, label="I1 + I2, V2 = 5")
+
+    mOffset = 18;
+    Date10 = plt.plot(Vdm4[mOffset:len(line3)-mOffset], line3[mOffset:len(line3)-mOffset], 'co',  markersize=3, label="slope = Gdm, V2=3")
+    Date11 = plt.plot(Vdm4[mOffset:len(line3)-mOffset], line4[mOffset:len(line4)-mOffset], 'go',  markersize=3, label="slope = Gdm, V2=4")
+    Date12 = plt.plot(Vdm4[mOffset:len(line3)-mOffset], line5[mOffset:len(line5)-mOffset], 'mo',  markersize=3, label="slope = Gdm, V2=5")
 
     plt.xlabel(xLabel)
     plt.ylabel(yLabel)
